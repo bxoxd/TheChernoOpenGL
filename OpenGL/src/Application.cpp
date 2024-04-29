0e44ed3290458c3588b4d81d5afaa222cbdaed3a
bxoxd <83289173+bxoxd@users.noreply.github.com>
2024-04-29 20:07:50 +0800

    P10 使用glGetError()处理错误

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -6,6 +6,25 @@
 #include <sstream>
 #include <string>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char *func, const char *file, int line)
+{
+    while (GLenum error = glGetError()) {
+        std::cout << "[OpenGL Error] (" << error << ") " << func << " " << file << " : " << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
 struct ShaderProgramSource
 {
     std::string VertexSource;
@@ -142,7 +161,9 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+        
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
